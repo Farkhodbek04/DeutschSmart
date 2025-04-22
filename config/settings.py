48b99c2d367936ac4a2d54eb455f400b1147d6c4
Farxodbek <farhodbekjaloldinov@gmail.com>
2025-04-22 16:15:44 +0500

    ready for deployment

--- config/settings.py
+++ config/settings.py
@@ -131,4 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://yourapp.jprq.site']
+# CSRF_TRUSTED_ORIGINS = ['https://yourapp.jprq.site']
