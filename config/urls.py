d920ac6f2718796099afb577fbfd8fbd0c6520a3
Farxodbek <farhodbekjaloldinov@gmail.com>
2025-04-23 17:22:09 +0500

    docker compose watch

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('main.urls'))
+    path('api/', include('main.urls')),
+    path('health/', health_check, name='health-check'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
